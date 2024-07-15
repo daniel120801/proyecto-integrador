33a5a16dcc580e5b94eaaa0f913ff1bc1d59e584
ReneT05 <trevi0527rene@gmail.com>
2024-07-15 14:10:17 -0500

    {

---
+++ codigo fuente/PHP/conection.php
@@ -0,0 +1,37 @@
+<?php
+
+class BD_PDO
+{
+    public function exec_instruction($instruction)
+    {
+        $host = "localhost";
+        $usr  = "root";
+        $pwd  = "";
+        $db   = "db_sushi";
+        
+        try {
+            $conexion = new PDO("mysql:host=$host;dbname=$db;", $usr, $pwd);
+            // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Failed to get DB handle: '.$e->getMessage();
+            exit;
+        }
+
+        // Asignando una instruccion sql
+        $result = [];
+        $result['message'] = 'vacio';
+        $result['rows'] = [];
+        $query = $conexion->prepare($instruction);
+        if (!$query) {
+            $result['message'] = 'Error al mostrar';
+        } else {
+            $query->execute();
+            while ($r = $query->fetch()) {
+                $result['rows'][] = $r;
+                $result['message'] = 'ejecutado con exito';
+            }
+        }
+
+        return $result;
+    }
+}
